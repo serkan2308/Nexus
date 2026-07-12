0138a728ae576724ea91dd19bcf86009cb7b183d
serkan2308 <m.serkandolek@gmail.com>
2026-07-12 13:46:26 +0300

    Create filtering_func.py

---
+++ filtering_func.py
@@ -0,0 +1,10 @@
+class FilteringFunction:
+    def __init__(self):
+        print("Filtering Function: Filtreleme modülü hazır.")
+
+    def filter(self, data):
+        # Hataları filtreleyip temiz veriyi ayırma
+        print("Filtering Function: Hatalar ayıklanıyor...")
+        
+        # Filtreleme sonucunu döndür
+        return "Clean data ready for Error Brain"
