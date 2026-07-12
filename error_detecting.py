a552034dbb945b8ab65a861c14539efb76eb5cc6
serkan2308 <m.serkandolek@gmail.com>
2026-07-12 13:39:15 +0300

    Implement ErrorDetectingEngine class

---
+++ error_detecting.py
@@ -0,0 +1,10 @@
+class ErrorDetectingEngine:
+    def __init__(self):
+        print("Error Detecting Engine: Devrede. Hatalar taranıyor...")
+
+    def detect(self, data):
+        # Hataları tespit eden mekanizma
+        print(f"Error Detecting Engine: {data} üzerinde tarama yapılıyor.")
+        
+        # Filtreleme fonksiyonuna aktarım hazırlığı
+        return "Data sent to Filtering Function"
